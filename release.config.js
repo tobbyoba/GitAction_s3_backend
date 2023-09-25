92181e141b5b92f4a25b3144cecee39246a04c76
olutoba obawemimo <tobbyoba86@gmail.com>
2023-09-25 12:54:04 -0500

    updated config.js

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 branches: "main",
-repositoryUrl: "https://github.com/tobbyoba/testGitaction.git",
+repositoryUrl: "https://github.com/tobbyoba/Gitaction_s3_backed.git",
  plugins: [
  '@semantic-release/commit-analyzer',
  '@semantic-release/release-notes-generator',
