6378bc55fa739ccd909242b5b64f241da7cef652
tobbyoba <tobbyoba86@gmail.com>
2023-10-12 16:20:45 -0500

    updating output

--- vpc2/outputs.tf
+++ vpc2/outputs.tf
@@ -7,5 +7,12 @@ output "public" {
   value = aws_subnet.public_subnet_cidr.*.id
 }
 
+output "node_role" {
+  value = module.IAM_module.node_role
 
+}
+
+output "demo_role" {
+  value = module.IAM_module.demo_role
+}
 
