9cdbf82911677a17520c06e6ec063247b19c4c60
tobbyoba <tobbyoba86@gmail.com>
2024-01-22 15:08:25 -0600

    feat:update s3 bucket
    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET>
    
    Problem
    Solution
    Note

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -7,9 +7,51 @@ resource "aws_s3_bucket" "backend" {
   }
 }
 
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.backend.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_public_access_block" "backend1" {
+  bucket = aws_s3_bucket.backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "backend2" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
 resource "aws_kms_key" "mykey" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket1" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
@@ -35,6 +77,13 @@ resource "random_integer" "backend" {
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.backend.id
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example2" {
+  bucket = aws_s3_bucket.log_bucket.id
   versioning_configuration {
     status = var.versioning
   }
